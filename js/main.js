2754763c9ebba91c1dda9eecac2b90fbd4561b2f
Samuel Luis dos Santos <mucasantos@gmail.com>
2023-12-15 21:52:28 -0300

    add main add btn

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
 import Kanban from "./view/Kanban.js";
+import Item from "./view/Item.js";
+
+
+const mainInput = document.querySelector("input")
+const addButton = document.querySelector(".button")
+
+addButton.addEventListener("click", () => {
+    const item = new Item(1, mainInput.value);
+})
 
 new Kanban(
     document.querySelector(".kanban")
